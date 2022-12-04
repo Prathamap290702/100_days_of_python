47d144bdd63c38faf095ffe7c2b57ae3bf27ee01
Prathamap290702 <prathamap2002@gmail.com>
2022-12-04 19:52:15 +0530

    DAY-03 :

--- treasure_island_game_Day-03.py
+++ treasure_island_game_Day-03.py
@@ -1,5 +1,5 @@
 # if__else\
-#odd or even
+# odd or even
 
 # num = int(input("Enter the number you want to check odd or even : "))
 # if num%2==0:
@@ -15,26 +15,82 @@
 # else:
 #     print("You can ride the Rollercoaster.")
 #     if age<12:
-#         print("Please pay $5.")
+#         bill = 5
+#         print("Child ticket is $5.")
 #     elif age <= 18:
-#         print("Please pay $7.")
+#         bill = 7
+#         print("Teenager ticket is $7.")
 #     else:
-#         print("Please pay $12.")
+#         bill = 12
+#         print("Adult ticket is $12.")
+#     ch = input("Do you want your Photo taken for only $3?? Y/N : ")
+#     if ch == 'Y':
+#         bill +=3
+#     else:
+#       pass
+#     print(f"You have to pay ${bill}.")
 
 # BMI 2.0
-height = float(input("What is your height in metres : "))
-weight = float(input("What is your weight in kgs : "))
-print(type(height))
-bmi = float(weight) / float(height ** 2)
-print("Your BMI is : " + str(bmi))
-if bmi < 18.5:
-    print("You are Underweight Category.")
-elif bmi < 25:
-    print("You are in Ideal weight Category.")
+# height = float(input("What is your height in metres : "))
+# weight = float(input("What is your weight in kgs : "))
+# print(type(height))
+# bmi = float(weight) / float(height ** 2)
+# print("Your BMI is : " + str(bmi))
+# if bmi < 18.5:
+#     print("You are Underweight Category.")
+# elif bmi < 25:
+#     print("You are in Ideal weight Category.")
 
-elif bmi < 30:
-    print("You are in Overweight Category.")
-elif bmi < 35:
-    print("You are in Obese Category")
-else :
-    print("You are Clinically Obese.")
+# elif bmi < 30:
+#     print("You are in Overweight Category.")
+# elif bmi < 35:
+#     print("You are in Obese Category")
+# # else :
+# #     print("You are Clinically Obese.")
+
+# Leap Year
+# year = int(input("Enter the year you want to check leap year : "))
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("Leap Year.")
+#         else:
+#             print("Not a Leap Year.")
+#     else:
+#         print("Leap Year.")
+# else:
+#     print("Not a Leap Year.")
+
+# if year % 400 == 0:
+#             print("Leap Year.")
+
+#PIZZA
+
+print("Welcome to the Python Pizza Deliveries 😄")
+size = input('''Which size of pizza do you want to try today?
+S - Small pizza  - $ 15
+M - Medium pizza - $ 20
+L - Large  Pizza - $ 25 
+''')
+pepp = input('''Do you want Pepperoni for your Pizza
+Pepperoni for Small Pizza  - $2
+Pepperoni for Medium Pizza - $3
+Pepperoni for Large Pizza  - $5
+(Y/N) :''')
+
+if size == 'S':
+    bill = 15
+    if pepp == 'Y':
+        bill+=2    
+elif size == 'M':
+    bill = 20
+    if pepp == 'Y':
+        bill+=3 
+elif size == 'L':
+    bill = 25
+    if pepp == 'Y':
+        bill+=5  
+ches = input("Do you want Extra Cheese on your Pizza for just $1 (Y/N) : ")
+if ches == 'Y':
+    bill+=1
+print(f"Total Bill = ${bill} \n Please visit again 😁 and have a nice day.")
