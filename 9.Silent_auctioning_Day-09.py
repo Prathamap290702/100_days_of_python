5775472badd00c76c9fb71b4d945848a8f2328b3
Prathamap290702 <prathamap2002@gmail.com>
2022-12-10 23:55:05 +0530

    Day-09 :

---
+++ 9.Silent_auctioning_Day-09.py
@@ -0,0 +1,26 @@
+# #Dictionary
+# programming_dictionary = {
+#     "Bug" :  "An error",
+#     "Function" : "that can be called again and again.",
+# }
+# #retrieving value
+# print(programming_dictionary["Bug"])
+
+# #adding new entry
+# programming_dictionary["loop"] ="the action of doing something again and again."
+# # print(programming_dictionary)
+
+# # empty_dict = {}
+# # programming_dictionary = {}
+# # print(programming_dictionary)#wiping dictionary
+
+# #edit dictionary
+# # programming_dictionary["Bug"] = "new value"
+# # print(programming_dictionary["Bug"])
+
+# #looping in dictionary
+# for keys in programming_dictionary:
+#     print(keys)
+#     print(programming_dictionary[keys])
+
+#grading program
