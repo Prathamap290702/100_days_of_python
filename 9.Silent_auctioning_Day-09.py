204e01f7b4e1435605c339b61fc8e388f6cc6670
Prathamap290702 <prathamap2002@gmail.com>
2022-12-11 11:20:35 +0530

    Day-09 :

--- 9.Silent_auctioning_Day-09.py
+++ 9.Silent_auctioning_Day-09.py
@@ -23,4 +23,25 @@
 #     print(keys)
 #     print(programming_dictionary[keys])
 
-#grading program
+# grading program
+# students_scores = {"S1": 99,
+#                    "S2": 100,
+#                    "S3": 75,
+#                    "S4": 54,
+#                    "S4": 84,
+#                    "S5" : 33}
+# students_grades = {}
+# for names in students_scores:
+#     if students_scores[names] > 90 and students_scores[names] <= 100:
+#         students_grades[names] = "A"
+#     elif students_scores[names] > 80 and students_scores[names] <= 90:
+#         students_grades[names] = "B"
+#     elif students_scores[names] > 70 and students_scores[names] <= 80:
+#         students_grades[names] = "C"
+#     elif students_scores[names] > 60 and students_scores[names] <= 70:
+#         students_grades[names] = "D"
+#     elif students_scores[names] > 50 and students_scores[names] <= 60:
+#         students_grades[names] = "E"
+#     else:
+#         students_grades[names] = "FAIL"
+# print(students_grades)
