22e43b2f24681e511843e846d3cea17b56594550
Prathamap290702 <prathamap2002@gmail.com>
2022-12-12 21:36:30 +0530

    Day-10 :

--- 10.Calculator_Day-10.py
+++ 10.Calculator_Day-10.py
@@ -8,7 +8,7 @@
 # name = format_name("pratham", "prajapatI")
 # print(name)
 
-#Days in months
+# Days in months
 # def leap_year(year):
 #     # is_leap_year = False
 #     if year % 4 == 0:
@@ -28,6 +28,7 @@
 
 
 # def days_of_months(year, month):
+#     """Takes month and year and returns the number of days in that particaular month."""#Docstring used for writing the documentation for any function
 #     leap = leap_year(year)
 #     if leap == False:
 #         days = day[month - 1]
@@ -41,3 +42,54 @@
 # month = int(input("Enter the month : "))
 # days = days_of_months(year, month)
 # print(f"The {month} of {year} has {days} days in it.")
+
+# Calculator
+
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def subtract(num1, num2):
+    return num1 - num2
+
+
+def divide(num1, num2):
+    return num1 / num2
+
+
+operations_list = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+def Calculate(): 
+    num1 = int(input("Enter the first number : "))
+    on = True
+    while on == True:
+        operations = input(('''Operations : +
+                    -
+                    *
+                    /\n : '''))
+        num2 = int(input("Enter the next number : "))
+        operate = operations_list[operations]
+        answer = operate(num1, num2)
+        print(f"{num1} {operations} {num2} = {answer}")
+        loop = int(input(f'''1. Do you want to continue with {answer}
+        2.You want to start new operation
+        3.You want to stop ? : '''))
+        if loop == 1:
+            num1 = answer
+        elif loop == 2:
+            Calculate()
+        elif loop == 3:
+            print("Thank you for using the calculator....")
+            on == False
+            return
+Calculate()
