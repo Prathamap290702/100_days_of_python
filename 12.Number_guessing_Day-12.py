d3fb15ee10922d4f6bbd8771d8b689a3bee06cd3
Prathamap290702 <prathamap2002@gmail.com>
2022-12-14 19:05:53 +0530

    Day-12 :

---
+++ 12.Number_guessing_Day-12.py
@@ -0,0 +1,45 @@
+# Scope
+
+# enemies = 1
+
+
+# def increase_enemies():
+#     enemies = 2
+#     print(f"Enemies inside the function are {enemies}")
+
+
+# increase_enemies()
+# print(f"Enemies outside the function are {enemies}")
+
+# Local scope
+
+# def drink_potion():
+#     potion_strength = 2
+#     print(potion_strength)
+
+# drink_potion()
+# print(potion_strenght) # this is wrong as it was not outside the function
+
+# Global scope
+# player_health = 10
+
+
+# def game():
+#  def drink_potion():
+#      potion_strength = 2
+#      print(player_health)
+
+#  drink_potion()
+# print(player_health)
+
+# There is no block
+# game_level = 3
+
+
+# def create_enemy():
+#     enemies = ["Skeleton", "Zombie", "Alien"]
+#     if game_level<5:
+#         new_enemy = enemies[0]
+        
+#     print(new_enemy)
+# create_enemy()
