a973a796050593dc717f59f0618d9db55758b220
Prathamap290702 <prathamap2002@gmail.com>
2022-12-15 14:08:01 +0530

    Day-13 : Completed

---
+++ 13.Debugging_Day-13.py
@@ -0,0 +1,11 @@
+#Contains various types of errors and how to solve it 
+
+#one of the most important way is to use print() function if there is an error print it 
+
+#Using a Debugger
+#We can use debugger to solve the bugs as it is more commplex
+#use www.pythontutor.com and click visualize 
+
+#ask stackoverflow
+
+#debugging odd even code, leap year code, buzz fizz code..
