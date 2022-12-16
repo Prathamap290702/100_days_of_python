bd04c490838f09fb3208fa5fecfeb1538891cf83
Prathamap290702 <prathamap2002@gmail.com>
2022-12-16 23:39:54 +0530

    Day-15 : Completed

--- 15.Coffee_machine_Day-15.py
+++ 15.Coffee_machine_Day-15.py
@@ -3,7 +3,7 @@ Menu = {
     "espresso": {
         "ingredients": {
             "water": 50,
-            "milk" : 0,
+            "milk": 0,
             "coffee": 18,
         },
         "cost": 1.5,
@@ -35,13 +35,13 @@ resources = {
 
 
 def pay(choice):
-    cash = 0
     print("Please insert coins.")
     quarters = int(input("How many quarters? : "))
     pennies = int(input("How many pennies? : "))
     dimes = int(input("How many dimes? : "))
     nickels = int(input("How many nickels? : "))
-    total = round(quarters * 0.25 + pennies * 0.01 + dimes * 0.1 + nickels * 0.05, 2)
+    total = round(quarters * 0.25 + pennies * 0.01 +
+                  dimes * 0.1 + nickels * 0.05, 2)
     if choice == 'espresso':
         cost = Menu['espresso']['cost']
     elif choice == 'latte':
@@ -54,8 +54,9 @@ def pay(choice):
         print("Sorry, thats not enough money.")
         return False
     elif cost <= total:
+        global cash 
         cash += cost
-        print(f"Enjoy your {choice}")
+        print(f"Enjoy your {choice}☕")
         if cost < total:
             print(f"Here's the change ${round(total-cost,2)}")
         return True
@@ -69,26 +70,35 @@ def serve(choice):
             resources['coffee'] = resources['coffee'] - 18
         elif choice == 'latte':
             resources['water'] = resources['water'] - 200
-            resources['water'] = resources['water'] - 150
+            resources['milk'] = resources['milk'] - 150
             resources['coffee'] = resources['coffee'] - 24
         elif choice == 'cappuccino':
             resources['water'] = resources['water'] - 250
-            resources['water'] = resources['water'] - 100
+            resources['milk'] = resources['milk'] - 100
             resources['coffee'] = resources['coffee'] - 24
+
+
+def is_enough(ingredients):
+    for item in ingredients: 
+        if ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough {item}")
+            return False
+        else:
+          return True
+
+
 loop = True
 while loop == True:
     type = input("What would you like? (Espresso/latte/cappuccino) : ").lower()
     if type == 'report':
-        print(f'''Water : {resources['water']}
-Coffee : {resources['coffee']}
-Milk : {resources['milk']}
+        print(f'''Water : {resources['water']}ml
+Coffee : {resources['coffee']}g
+Milk : {resources['milk']}ml
 Money : ${cash}''')
     elif type == 'off':
         loop = False
         print("Turning off")
     else:
-        if resources['water'] >= Menu[type]['ingredients']['water'] and resources['coffee'] >= Menu[type]['ingredients']['coffee'] and resources['milk'] >= Menu[type]['ingredients']['milk']:
-         serve(type)
-        else:
-           print("Not Enough Resources.")
-           loop = False 
+        if is_enough(Menu[type]['ingredients']):
+            serve(type) 
+            loop = False
