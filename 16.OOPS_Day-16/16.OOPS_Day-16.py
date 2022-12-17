06949b74534a61e6e4ca69765d8bdc63141c68b3
Prathamap290702 <prathamap2002@gmail.com>
2022-12-17 23:33:05 +0530

    Day-16 :

---
+++ 16.OOPS_Day-16/16.OOPS_Day-16.py
@@ -0,0 +1,21 @@
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("green","red")
+# timmy.forward(100)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+# from prettytable import PrettyTable
+# table = PrettyTable()
+# table.add_column("POkeMon Name", ["Pikachu", "charizard", "Squirtle"])
+# table.add_column("Type", ["Electric", "Fire", "Water"])
+# table.align = "l"
+# print(table)
+
+#Coffee machine using OOPS
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
