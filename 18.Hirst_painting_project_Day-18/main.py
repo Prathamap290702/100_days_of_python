3aab2afedb1dae359425a32ef2461b6ad5a999fd
Prathamap290702 <prathamap2002@gmail.com>
2022-12-23 23:50:57 +0530

    Day-18 :

--- 18.Hirst_painting_project_Day-18/main.py
+++ 18.Hirst_painting_project_Day-18/main.py
@@ -1,9 +1,28 @@
 from turtle import Turtle, Screen
-timmy = Turtle()
-timmy.shape("turtle")
-timmy.color("red")
-timmy.forward(100)
 
+# import turtle
+# tim = turtle.Turtle()//every time we have to write turtle.
+
+# from turtle import * //import everything
+
+#import turtle as t //alias
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("red")
+# tim.forward(100)
+
+#make square
+# for i in range(0,4):
+#     tim.forward(100)
+#     tim.left(90)
+
+#dashed lines
+for _ in range(0,15):
+    tim.forward(10)
+    tim.pu()
+    tim.forward(10)
+    tim.pd()
 
 
 
