257c64a57c132e271dcbeb83a33ee99ee81698fe
Prathamap290702 <prathamap2002@gmail.com>
2022-12-23 23:33:57 +0530

    Day-18 :

---
+++ 18.Hirst_painting_project_Day-18/main.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+timmy.forward(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
