0ec63e7a4836c094b3db30587b6fb2b956f140ef
Prathamap290702 <prathamap2002@gmail.com>
2022-12-24 23:54:56 +0530

    Day-18 :

--- 18.Hirst_painting_project_Day-18/main.py
+++ 18.Hirst_painting_project_Day-18/main.py
@@ -1,43 +1,60 @@
-from turtle import Turtle, Screen
-
+import turtle as t
+import random
 # import turtle
 # tim = turtle.Turtle()//every time we have to write turtle.
 
 # from turtle import * //import everything
 
-#import turtle as t //alias
+# import turtle as t //alias
 
-tim = Turtle()
+tim = t.Turtle()
 tim.shape("turtle")
-tim.color("red")
+t.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    colours = (r, g, b)
+    return colours
 # tim.forward(100)
 
-#make square
+# make square
 # for i in range(0,4):
 #     tim.forward(100)
 #     tim.left(90)
 
-#dashed lines
+# dashed lines
 # for _ in range(0,15):
 #     tim.forward(10)
 #     tim.pu()
 #     tim.forward(10)
 #     tim.pd()
 
-#drawing different shapes
-def draw(sides):
-    for i in range(0,sides):
-        tim.forward(100)
-        tim.left(360/sides)
-
-for i in range (3,11):
-    draw(i)
+# drawing different shapes
 
 
+# def draw(sides):
+#     for i in range(0, sides):
+#         tim.forward(100)
+#         tim.left(360/sides)
 
 
+# colors = ["medium orchid", "chartreuse", "cyan", "red", "blue",
+#           "yellow", "deep sky blue", "dark violet", "aquamarine"]
+# for i in range(3, 11):
+#     tim.color(random.choice(colors))
+#     draw(i)
+direction = [0, 90, 180, 270]
 
-
+for i in range(0, 200):
+    tim.pensize(15)
+    tim.speed(10)
+    tim.color(random_color())
+    tim.forward(20)
+    tim.setheading(random.choice(direction))
+    # tim.forward(20)
 
 screen = Screen()
 screen.exitonclick()
