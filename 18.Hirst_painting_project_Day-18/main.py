9f5f01ee3ef2d89690058c47aa5f5749dbefa477
Prathamap290702 <prathamap2002@gmail.com>
2022-12-30 00:42:26 +0530

    Day-18 :

--- 18.Hirst_painting_project_Day-18/main.py
+++ 18.Hirst_painting_project_Day-18/main.py
@@ -1,16 +1,16 @@
-# import turtle as t
-# import random
+import turtle as t
+import random
 # import colorgram
 # # import turtle
-# # tim = turtle.Turtle()//every time we have to write turtle.
+# tim = turtle.Turtle() #every time we have to write turtle.
 
 # # from turtle import * //import everything
 
-# # import turtle as t //alias
+import turtle as t #alias
 
-# # tim = t.Turtle()
-# # tim.shape("turtle")
-# # t.colormode(255)
+tim = t.Turtle()
+tim.shape("turtle")
+t.colormode(255)
 
 
 # # def random_color():
@@ -67,8 +67,6 @@
 # #         tim.setheading(tim.heading() + size_of_gap)
         
 # # draw_spirograph(5)
-# # screen = Screen()
-# # screen.exitonclick()
 # rgb_colors = []
 # colors = colorgram.extract("download.jpg", 30)
 # for color in colors:
@@ -81,3 +79,10 @@
     
 # print(rgb_colors)
 color_list = [(225, 229, 237), (241, 234, 215), (242, 229, 238), (224, 239, 231), (191, 166, 121), (142, 165, 189), (136, 94, 57), (63, 100, 135), (219, 207, 130), (13, 23, 55), (183, 149, 168), (144, 175, 154), (56, 22, 10), (172, 152, 50), (51, 13, 27), (74, 116, 85), (124, 80, 99), (12, 35, 20), (178, 185, 217), (30, 51, 120), (212, 179, 195), (171, 205, 185), (159, 106, 133), (101, 120, 171), (116, 32, 49), (95, 152, 110), (123, 36, 23), (30, 86, 61), (165, 202, 211), (172, 106, 94)]
+tim.setheading(225)
+for i in range(10):
+    tim.dot(20,random.choice(color_list))
+    tim.forward(50)
+
+screen = t.Screen()
+screen.exitonclick() 
