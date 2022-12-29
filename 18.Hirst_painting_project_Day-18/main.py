1b86c665eef882ed66dbef14b8be9028e0cbeafb
Prathamap290702 <prathamap2002@gmail.com>
2022-12-29 23:53:33 +0530

    Day-18 :

--- 18.Hirst_painting_project_Day-18/main.py
+++ 18.Hirst_painting_project_Day-18/main.py
@@ -8,17 +8,17 @@ import colorgram
 
 # import turtle as t //alias
 
-tim = t.Turtle()
-tim.shape("turtle")
-t.colormode(255)
+# tim = t.Turtle()
+# tim.shape("turtle")
+# t.colormode(255)
 
 
-def random_color():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    colours = (r, g, b)
-    return colours
+# def random_color():
+#     r = random.randint(0, 255)
+#     g = random.randint(0, 255)
+#     b = random.randint(0, 255)
+#     colours = (r, g, b)
+#     return colours
 # tim.forward(100)
 
 # make square
@@ -47,7 +47,7 @@ def random_color():
 # for i in range(3, 11):
 #     tim.color(random.choice(colors))
 #     draw(i)
-direction = [0, 90, 180, 270]
+# direction = [0, 90, 180, 270]
 
 # for i in range(0, 200):
 #     tim.pensize(15)
@@ -57,7 +57,7 @@ direction = [0, 90, 180, 270]
 #     tim.setheading(random.choice(direction))
 #     # tim.forward(20)
 
-tim.speed(20)
+# tim.speed(20)
 
 
 # def draw_spirograph(size_of_gap):
@@ -67,5 +67,9 @@ tim.speed(20)
 #         tim.setheading(tim.heading() + size_of_gap)
         
 # draw_spirograph(5)
-screen = Screen()
-screen.exitonclick()
+# screen = Screen()
+# screen.exitonclick()
+
+colors = colorgram.extract("download.jpg", 30)
+for color in colors:
+    
