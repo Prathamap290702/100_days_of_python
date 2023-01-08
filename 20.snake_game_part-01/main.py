a9e4769d2c0f138fe740c8add36e6c7397aec5d7
Prathamap290702 <prathamap2002@gmail.com>
2023-01-08 19:28:39 +0530

    Day-20 :

--- 20.snake_game_part-01/main.py
+++ 20.snake_game_part-01/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 
 
@@ -7,31 +8,18 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Nokia Snake in python")
 screen.tracer(0)
-pos = [(0,0), (-20,0), (-40,0)]
 
-segments = []
-for positions in pos:
-    new_segment= Turtle("square")
-    new_segment.penup()
-    new_segment.color("white")
-    new_segment.goto(positions)
-    segments.append(new_segment)
-    
+snake = Snake()
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    
-    for seg_num in range(2,0,-1):
-        new_x = segments[seg_num-1].xcor()
-        new_y = segments[seg_num-1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-        
-    segments[0].forward(20)
-
-
-
-
+    snake.move()
 
 screen.exitonclick() 
+
+
+
+
+
